f10f39f66e7d4b6bc1fb87c2970b553b94c472c9
Konstantin <konstantinchirgin@gmail.com>
2021-09-08 20:08:53 +0300

    Adding neuronet

--- telegram bot/scr/message_options.py
+++
@@ -1,7 +0,0 @@
-def lower_registr(message: str) -> str:
-    """
-    Function for make message in lower registr
-    :param message:str-any message
-    :return:str-same message in low registr
-    """
-    return message.lower()
